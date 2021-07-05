c69cce61f5c3a18fe0b25d26fb61e233f4371ad4
Chris Cui <cuich01@gmail.com>
2021-07-05 12:46:27 +1000

    move

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SwaggerComponent } from './swagger/swagger.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'specifications',
+    component: SwaggerComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
